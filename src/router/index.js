8f67c92fc1edff513d1fc70c05e5f62f5ce0ce03
sameer <sameergupta1499@gmail.com>
2023-10-03 21:30:08 +0530

    added / in index.js

--- src/router/index.js
+++ src/router/index.js
@@ -33,7 +33,7 @@ import HomePage from '../views/HomePage'
 const router = createRouter({
     history: createWebHistory("/portfolio-project/"),
     routes: [
-        { path: '', component: HomePage },
+        { path: '/', component: HomePage },
         // { path: '/accessories/', component: ProductPage, props: { page: 'accessories' } },
     ]
 })
